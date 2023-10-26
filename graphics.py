0403cc2ffdbfb714536afc8f84a6585917ea75cb
nirogu <nirogu@outlook.com>
2023-10-25 23:50:04 -0500

    Improve graphic

--- graphics.py
+++ graphics.py
@@ -13,5 +13,12 @@ parser.add_argument("output_file", help="File where graphic will be saved.")
 args = parser.parse_args()
 
 data = pd.read_csv(args.input_file)
-ax = sns.lineplot(data, x="Matrix_Size", y="Time (microseconds)", hue="N_Threads")
+data["N_Threads"] = data["N_Threads"].astype(str)
+data["Time (secs)"] = data["Time"] * 10**-6
+ax = sns.lineplot(
+    data, x="Matrix_Size", y="Time (secs)", hue="N_Threads", palette="plasma"
+)
+ax.grid(axis="both", which="both", linestyle="--")
+ax.set_title("Matrix size vs Time for every number of threads")
+ax.set_xlim(200, 2000)
 plt.savefig(args.output_file)
