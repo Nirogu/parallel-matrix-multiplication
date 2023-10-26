5131d459df7c3bea0e79e2fbec8c82764030db3d
nirogu <nirogu@outlook.com>
2023-10-26 00:47:21 -0500

    Add threads-time graphic

--- graphics.py
+++ graphics.py
@@ -9,16 +9,28 @@ parser.add_argument(
     "input_file",
     help="CSV file to be read and processed. Should be output of launcher.py",
 )
-parser.add_argument("output_file", help="File where graphic will be saved.")
 args = parser.parse_args()
 
-data = pd.read_csv(args.input_file)
-data["N_Threads"] = data["N_Threads"].astype(str)
+input_file = args.input_file
+data = pd.read_csv(input_file)
 data["Time (secs)"] = data["Time"] * 10**-6
+
+ax = sns.lineplot(
+    data, x="N_Threads", y="Time (secs)", hue="Matrix_Size", palette="plasma"
+)
+ax.set_xticks(range(2, 21, 2))
+ax.set_xlim(2, 20)
+ax.grid(axis="both", which="both", linestyle="--")
+ax.set_title("Number of threads vs Time for some matrix sizes")
+plt.savefig("threads-time.png")
+plt.clf()
+plt.cla()
+
+data["N_Threads"] = data["N_Threads"].astype(str)
 ax = sns.lineplot(
     data, x="Matrix_Size", y="Time (secs)", hue="N_Threads", palette="plasma"
 )
 ax.grid(axis="both", which="both", linestyle="--")
 ax.set_title("Matrix size vs Time for every number of threads")
 ax.set_xlim(200, 2000)
-plt.savefig(args.output_file)
+plt.savefig("size-time.png")
